1bf285bb5878ec29f33e1a2206b95fa4b6cdc6e6
Avneet kaur <avneetbagga06@gmail.com>
2023-10-05 13:56:17 +0530

    Initial commit

---
+++ Index.php
@@ -0,0 +1,93 @@
+﻿<!DOCTYPE html>
+<?php 
+session_start();
+?>
+<html>
+<head>
+    <title>Qnline Question paper</title>
+    <link type="text/css" rel="stylesheet" href="StyleSheet1.css" />
+</head>
+<body>
+
+<?php
+ include ('header.php');?>
+</div>  
+ <div id="bottom">
+<br>
+<br>
+<div id="boxes">
+<marquee  behavior="alternate" width="100%" scrolldelay="200" >
+<h3>UNIQUE FEATURES</h3>
+</marquee>
+<ol>
+<li type="square">
+A Customised solution
+</li>
+<li type="square">
+Generates a set of various types of answers.
+</li>
+<li type="square">
+MCA Question paper
+</li>
+<li type="square">
+Questions are based on learning objectives
+</li>
+<li type="square">
+Can add user defined questions.
+</li>
+
+</ol>
+
+</div>
+<div id="boxes">
+<marquee  behavior="alternate" width="100%" scrolldelay="200" >
+
+<h3>NEWS & EVENTS</h3>
+</marquee>
+<ol>
+<li type="square">
+Date : 24-03-2022
+Qpgen.Com - Mini Project Exam
+</li>
+<li type="square">
+Date : 24-03-2022
+Complition Date of the project
+</li>
+</ol>
+</div>
+
+<div id="boxes">
+<marquee  behavior="alternate" width="100%" scrolldelay="200" >
+
+<h3>FEEDBACK CORNER</h3>
+</marquee>
+<u>Teachers Feedback</u>
+<ol>
+<li type="square">
+A brilliant effort!!  
+
+</li>
+<li type="square">
+Now we will have lots of time to utilize for other constructive work.</li>
+<li type="square">
+It is something that we always required.</li>
+
+</ol>
+</div>
+
+<div id="boxes1">
+                                    
+<img height="200" width="1300" src="images.jpg"/>
+
+
+</div>
+
+
+ </div>
+
+<div id="footer">
+Content goes here</div>
+
+ <br/>
+</body>
+</html>
---
+++ aboutus.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php
+session_start();
+?>
+<html>
+<head>
+    <title>Sign up page</title>
+</head>
+<body>
+<?php
+ include ('header.php');?>
+
+<div id="bottom">
+<hr>
+<h1><u>ABOUT US!</u></h1>
+<hr>
+<p>This system is made to increase the interaction between the students and the teachers.
+</p>
+<p>
+This project has been made under the guidence of Dr.Tapant Kant Sir.
+</p>
+<p>
+The team members of the projects were Liza,Pooja,Shilpee,Roshini,Areeba ,Vani,Amita and Avneet
+</p>
+
+
+</div>
+</body>
+</html>
